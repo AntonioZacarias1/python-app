80c59964cb0c0998c8052682ed73df0f39f4587f
AntonioZacarias1 <javierzacariasperez@gmail.com>
2026-07-06 08:51:53 -0700

    added CI workflow

--- src/app.py
+++ src/app.py
@@ -11,7 +11,8 @@ app = Flask(__name__)
 def details():
     return jsonify({
         'time': datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-        'hostname': socket.gethostname()
+        'hostname': socket.gethostname(),
+        'message': 'hello world'
     })
 
 @app.route('/api/v1/healthz')
